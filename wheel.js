fa36f10c69c62c78a66debf05039ca24b93f0a6e
George Kamau <alphons3k@yahoo.com>
2018-09-15 20:18:09 -0400

    wheel of fortune, style game

---
+++ wheel.js
@@ -0,0 +1,128 @@
+//  WORDS
+var array_of_words =["trump", "car", "nycda", "washington", "money", "elephant"];
+var hints= {
+    trump:['president', 'troll','billionare'],
+    car: ['drive', 'wheels','stereo'],
+    nycda: ['college', 'design','newyork'],
+    washington: ['white house', '1600 pen ave', 'lincoln memorial'],
+    money: ['green', 'paper', 'bills'],
+    elephant: ['big animal', 'trunks','africa']
+}
+ 
+ 
+
+// CURRENT GAME WORD
+var randm = Math.floor(Math.random()*array_of_words.length); 
+var game_word = array_of_words[randm];
+ 
+
+// HINT GENERATOR
+function hin(){
+    var randh = Math.floor(Math.random()*3); 
+    var randHint = hints[game_word][randh]; 
+    document.querySelector("#hint").innerHTML = randHint; 
+    document.querySelector("#hint").style.backgroundColor = "lightyellow";
+    }
+      
+ 
+ 
+
+
+// VARIABLES
+var char = '';
+var entered_char='';
+var score =0;
+var doubleGameWord=game_word.split("");
+
+ 
+
+// GET CHAR FROM INPUT
+function fecth_char() {
+ char= document.getElementById("char").value.toLowerCase();  
+ match_char(char, game_word); 
+}
+
+
+
+//  MATCH INPUT CHAR WITH CURRENT WORD
+function match_char(char, game_word){
+    var i=0;
+    while (i< game_word.length){
+        if(game_word[i].toLowerCase() == char){ 
+            showDiv(char)
+            score+=10;
+            upScore(score); 
+        } else {
+            score-=1;
+            upScore(score);
+            if(score<-20){ 
+                hin();
+            } 
+        }
+        i++;
+    }
+
+}
+ 
+
+// UPDATE SCORE
+function upScore(j) {
+    if (score>1) {
+        document.querySelector("#score").style.backgroundColor = "green";
+        document.querySelector("#score").style.color = "white";
+}
+    if (score<1) {
+        document.querySelector("#score").style.backgroundColor = "red";
+        document.querySelector("#score").style.Color = "white";
+    }
+    document.querySelector("#score").innerHTML = j;
+}
+
+
+
+// START GAME BY ASSINING WORD
+function onLod (){
+    var i=0;
+    var j=0;
+    var tt= game_word.length;
+  
+     
+    while (i<tt){
+    $('.words').append("<div class='mainInn' onclick='myShow(this)' )>" + game_word[i] + "</div>");
+    i++;
+    }
+    
+}
+
+
+//  SHOW CORRECT NUMBER
+function showDiv(w){
+    var i =0;
+    while (i<game_word.length){
+         var x = document.querySelectorAll(".mainInn");  
+         if (x[i].innerHTML == w)
+         x[i].style.backgroundColor = "white"; 
+         delet(w);
+        i++;
+    }
+
+}
+
+  
+ 
+// RESTART GAME LOGIC
+function delet(x){
+    while(doubleGameWord.indexOf(x) !==-1){
+        doubleGameWord.splice(doubleGameWord.indexOf(x),1);
+    }
+    if(doubleGameWord.length ==0){
+        setTimeout(wait, 3000); 
+    } 
+}
+
+
+// END RESTART TIMER
+function wait(){
+    location.reload();
+}
+ 
